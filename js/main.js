0d1cd86a24a9c02c86c48d5805a9a51ea380ae18
unknown <logicerpsolutions1990@gmail.com>
2016-04-22 11:57:24 +0530

    Initial Set up

---
+++ js/main.js
@@ -0,0 +1,56 @@
+(function() {
+  'use strict';
+
+  /**
+   * Hi! Thanks for using XSplit JS Framework :)
+   *
+   * XSplit JS Framework exposes useful methods to work with XSplit without 
+   * fiddling too much with the raw core methods exposed to JavaScript, which 
+   * makes developing plugins for XSplit a lot easier.
+   *
+   * To get started, you just have to require our library:
+   * var xjs = require('xjs');
+   *
+   * You can read more about it at our documentation:
+   * http://xjsframework.github.io/quickstart.html
+   * http://xjsframework.github.io/tutorials.html
+   * http://xjsframework.github.io/api.html
+   */
+   
+   
+
+
+  
+  var xjs = require('xjs');
+
+  /*xjs.ready().then(function() {
+    // Write your code here
+  });*/
+  
+  xjs.ready().then(function() { 
+
+xjs.Scene.getActiveScene()
+.then(function(scene) {
+alert('hi');
+
+});
+  var Scene = xjs.Scene.getById(1);
+
+Scene.getSources().then(function(sources) {
+
+  if (sources.length === 0) return;
+
+  // There's a valid source, let's use that
+  var source = sources[sources.length - 1];
+
+  
+  
+  return source.setCustomName('SourceTesting');
+}).then(function(source) {
+
+ //source.duplicate();
+ 
+});
+  
+  
+})})();
